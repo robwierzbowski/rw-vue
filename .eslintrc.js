2502cac167174cce94da852866a82c1bd8dc41b1
Rob Wierzbowski <rob.wierzbowski@gmail.com>
2017-09-02 13:14:06 -0400

    Format eslint file in AirBnB style

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,28 +12,35 @@ module.exports = {
   extends: 'airbnb-base',
   // required to lint *.vue files
   plugins: [
-    'html'
+    'html',
   ],
   // check if imports actually resolve
   'settings': {
     'import/resolver': {
       'webpack': {
-        'config': 'build/webpack.base.conf.js'
-      }
-    }
+        'config': 'build/webpack.base.conf.js',
+      },
+    },
   },
   // add your custom rules here
   'rules': {
     // don't require .vue extension when importing
-    'import/extensions': ['error', 'always', {
-      'js': 'never',
-      'vue': 'never'
-    }],
+    'import/extensions': [
+      'error',
+      'always',
+      {
+        'js': 'never',
+        'vue': 'never',
+      },
+    ],
     // allow optionalDependencies
-    'import/no-extraneous-dependencies': ['error', {
-      'optionalDependencies': ['test/unit/index.js']
-    }],
+    'import/no-extraneous-dependencies': [
+      'error',
+      {
+        'optionalDependencies': ['test/unit/index.js'],
+      },
+    ],
     // allow debugger during development
-    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
-  }
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
+  },
 }
