4c73dfae083108b7080b2f9224cf88ae687d7c0f
Rob Wierzbowski <rob.wierzbowski@gmail.com>
2017-09-03 15:34:34 -0400

    Fix test/ lint errors
    Snuck some code simplification in there too... :O

--- test/e2e/custom-assertions/elementCount.js
+++ test/e2e/custom-assertions/elementCount.js
@@ -1,26 +1,30 @@
 // A custom Nightwatch assertion.
-// the name of the method is the filename.
-// can be used in tests like this:
+// The assertion name is the filename.
+// Example usage:
 //
 //   browser.assert.elementCount(selector, count)
 //
-// for how to write custom assertions see
+// For more information on custom assertions see:
 // http://nightwatchjs.org/guide#writing-custom-assertions
+
 exports.assertion = function (selector, count) {
-  this.message = 'Testing if element <' + selector + '> has count: ' + count;
+  this.message = `Testing if element <${selector}> has count: ${count}`;
   this.expected = count;
-  this.pass = function (val) {
-    return val === this.expected;
-  }
-  this.value = function (res) {
-    return res.value;
-  }
+
+  this.pass = function (value) {
+    return value === this.expected;
+  };
+
+  this.value = function (result) {
+    return result.value;
+  };
+
   this.command = function (cb) {
-    var self = this;
-    return this.api.execute(function (selector) {
-      return document.querySelectorAll(selector).length;
-    }, [selector], function (res) {
-      cb.call(self, res);
-    });
-  }
-}
+    return this.api.execute(
+      selectorToCount => document.querySelectorAll(selectorToCount).length,
+      [selector],
+      (result) => {
+        cb(result);
+      });
+  };
+};
