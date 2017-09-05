f45dcfe95326e3155dc80ca2da8f020b89570e53
Rob Wierzbowski <rob.wierzbowski@gmail.com>
2017-09-05 14:25:28 -0400

    Create skeleton fancy-link component

---
+++ src/components/FancyLink.vue
@@ -0,0 +1,25 @@
+<template>
+  <a v-bind:href="href">
+    {{ content }}
+  </a>
+</template>
+
+<script>
+
+export default {
+  name: 'fancy-link',
+  props: ['href', 'content'],
+};
+
+</script>
+
+<style scoped>
+a {
+  text-decoration: none;
+  border-bottom: black 1px solid;
+}
+
+a:hover {
+  border-bottom-color: pink;
+}
+</style>
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,14 +1,21 @@
 <template>
   <div class="Home">
     <h1>{{ msg }}</h1>
+
+    <fancy-link href='foo' content='bar'></fancy-link>
   </div>
 </template>
 
 <script>
+import FancyLink from '@/components/FancyLink';
+
 const name = 'Robert';
 
 export default {
   name: 'home',
+  components: {
+    FancyLink,
+  },
   data() {
     return {
       msg: `Welcome Home, ${name}`,
