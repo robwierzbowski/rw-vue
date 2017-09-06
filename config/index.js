b16b3a1f088cf45c6f9f1b6f1f0d04ecca815684
Rob Wierzbowski <rob.wierzbowski@gmail.com>
2017-09-06 16:30:06 -0400

    Remove jsSourcemap setting

--- config/index.js
+++ config/index.js
@@ -12,7 +12,6 @@ module.exports = {
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
-    jsSourcemap: true,
     // Gzip off by default as many popular static hosts such as
     // Surge or Netlify already gzip all static assets for you.
     // Before setting to `true`, make sure to:
@@ -31,12 +30,6 @@ module.exports = {
     env: devEnv,
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
-    // CSS Sourcemaps off by default because relative paths are "buggy"
-    // with this option, according to the CSS-Loader README
-    // (https://github.com/webpack/css-loader#sourcemaps)
-    // In our experience, they generally work as expected,
-    // just be aware of this issue when enabling this option.
-    jsSourcemap: true,
   },
   test: {
     env: testEnv,
