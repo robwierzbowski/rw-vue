c7c33cc0bfd561ea71fb7ff0bc9b7f9567805759
Rob Wierzbowski <rob.wierzbowski@gmail.com>
2017-09-06 11:19:42 -0400

    Replace url-loader with file-loader

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -49,26 +49,26 @@ module.exports = {
       },
       {
         test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
-        loader: 'url-loader',
+        loader: 'file-loader',
         options: {
-          limit: 10000,
-          name: utils.assetsPath('img/[name].[hash:7].[ext]'),
+          name: '[name].[hash:7].[ext]',
+          outputPath: utils.assetsPath('img/'),
         },
       },
       {
         test: /\.(mp4|webm|ogg|mp3|wav|flac|aac)(\?.*)?$/,
-        loader: 'url-loader',
+        loader: 'file-loader',
         options: {
-          limit: 10000,
-          name: utils.assetsPath('media/[name].[hash:7].[ext]'),
+          name: '[name].[hash:7].[ext]',
+          outputPath: utils.assetsPath('media/'),
         },
       },
       {
         test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
-        loader: 'url-loader',
+        loader: 'file-loader',
         options: {
-          limit: 10000,
-          name: utils.assetsPath('fonts/[name].[hash:7].[ext]'),
+          name: '[name].[hash:7].[ext]',
+          outputPath: utils.assetsPath('fonts/'),
         },
       },
     ],
