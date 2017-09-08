24dfadaa4330c52b9b4a7f72eb32cfb6c7169a9d
Rob Wierzbowski <rob.wierzbowski@gmail.com>
2017-09-08 16:53:54 -0400

    Add skeleton ContactList component

---
+++ src/components/ContactList.vue
@@ -0,0 +1,42 @@
+<template>
+  <ul class='ContactList'>
+    <li class='ContactList-item' v-for="contact in contacts">
+     <fancy-link v-bind:href='contact.href' v-bind:content='contact.content' />
+    </li>
+  </ul>
+</template>
+
+<script>
+import FancyLink from '@/components/FancyLink';
+
+export default {
+  name: 'contact-list',
+  components: {
+    FancyLink,
+  },
+  data() {
+    return {
+      contacts: [
+        {
+          href: 'mailto:rob.wierzbowski@gmail.com',
+          content: 'rob.wierzbowski@gmail.com',
+        },
+        {
+          href: 'https://twitter.com/robwierzbowski',
+          content: '@robwierzbowski',
+        },
+        {
+          href: 'https://github.com/robwierzbowski',
+          content: 'robwierzbowski',
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style scoped>
+.ContactList {
+  list-style-type: none;
+}
+</style>
