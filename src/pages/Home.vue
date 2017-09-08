61bd59d352e464c7f4eb790ec93c55883fb5f798
Rob Wierzbowski <rob.wierzbowski@gmail.com>
2017-09-08 16:54:16 -0400

    Update Home component with 'more real' content

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,24 +1,30 @@
 <template>
   <div class="Home">
-    <h1>{{ msg }}</h1>
+    <h1>{{name}}</h1>
 
-    <fancy-link href='foo' content='bar'></fancy-link>
+    <p>I'm a design-minded developer focusing on front-end systems, process, and teamwork. I automate everything, love the good parts of JavaScript, and actually think CSS is awesome.</p>
+
+    <p>You can find me living and working in Pittsburgh, PA.</p>
+
+    <p>I'm free for consulting and full time remote work in 2017.</p>
+
+    <contact-list></contact-list>
   </div>
 </template>
 
 <script>
-import FancyLink from '@/components/FancyLink';
+import ContactList from '@/components/ContactList';
 
-const name = 'Robert';
+const name = 'Robert Wierzbowski';
 
 export default {
   name: 'home',
   components: {
-    FancyLink,
+    ContactList,
   },
   data() {
     return {
-      msg: `Welcome Home, ${name}`,
+      msg: name,
     };
   },
 };
