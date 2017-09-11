498f33a8fe4713162414bfe459d383fbebe699ec
Rob Wierzbowski <rob.wierzbowski@gmail.com>
2017-09-11 14:31:22 -0400

    Add title and description updater to router

--- src/router/index.js
+++ src/router/index.js
@@ -4,12 +4,31 @@ import Home from '@/pages/Home';
 
 Vue.use(Router);
 
-export default new Router({
+function updateMeta(route) {
+  const description = document.head.querySelector('meta[name=description]');
+
+  document.title = route.meta.description;
+  description.setAttribute('content', route.meta.title);
+}
+
+const router = new Router({
   routes: [
     {
       path: '/',
       name: 'Home',
       component: Home,
+
+      meta: {
+        title: 'A place for Rob',
+        description: 'Damn dog, look at all that Rob',
+      },
     },
   ],
 });
+
+router.beforeEach((to, from, next) => {
+  updateMeta(to);
+  next();
+});
+
+export default router;
