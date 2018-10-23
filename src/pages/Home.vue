ce2cc7219ad68059a2f0fde6fa29d1e68b5fe359
Rob Wierzbowski <rob.wierzbowski@gmail.com>
2018-10-22 22:19:12 -0400

    I'm just a developer

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -2,7 +2,7 @@
   <article class='Home'>
     <p>I'm a design-minded developer focusing on front-end systems, process, and teamwork. I automate everything, love the good parts of JavaScript, and actually think CSS is awesome.</p>
 
-    <p>I work as a lead front-end developer at <a href="https://www.stitchfix.com">Stitch Fix</a>. When I'm not escaping to some other metropolis, you'll find me at home in Pittsburgh, PA.</p>
+    <p>I work as a lead developer at <a href="https://www.stitchfix.com">Stitch Fix</a>. When I'm not escaping to some other metropolis, you'll find me at home in Pittsburgh, PA.</p>
   </article>
 </template>
 
