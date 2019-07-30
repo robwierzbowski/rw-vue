deb88ef7bb8aeca15179d821600eebd82cea5752
Rob Wierzbowski <rob.wierzbowski@gmail.com>
2019-07-29 22:35:28 -0400

    Fix spelling errors

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <article class='Home'>
     <p>
-      As a brand-focused engineer, I grow teams and lead technical intiatives that move the idea of a product forward. I bring fifteen years of cross-disiplinary experience and a developed understanding of technical, tactical, and organizational interconenectedness to the groups I work with.
+      As a brand-focused engineer, I grow teams and lead technical initiatives that move the idea of a product forward. I bring fifteen years of cross-disciplinary experience and a developed understanding of technical, tactical, and organizational interconnectedness to the groups I work with.
     </p>
 
     <p>
